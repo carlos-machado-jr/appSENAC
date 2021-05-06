19bbbae7f6269208392466bf6032f90cb493f95c
Técnico Informática <51121742019.1@senacrjedu.local>
2021-05-05 21:01:44 -0300

    pagina de resultado criada

--- src/app/pages/navegacao/navegacao.page.ts
+++ src/app/pages/navegacao/navegacao.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-navegacao',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavegacaoPage implements OnInit {
 
-  constructor() { }
+  constructor(private navegacao: NavController,
+              private alertController: AlertController) { }
 
   ngOnInit() {
   }
 
+  mostrarResultado(){
+    this.presentAlert()
+    this.navegacao.navigateForward("resultado");
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'A soma de 5 + 5 é 10',
+      // message: 'A soma de 5 + 5 é 10',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+  
 }
