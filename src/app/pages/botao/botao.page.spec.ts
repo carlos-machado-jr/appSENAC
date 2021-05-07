ee372638b9056bbcfcf9fc9857b8c2e44b3ffcb9
Técnico Informática <51121742019.1@senacrjedu.local>
2021-05-07 19:01:22 -0300

    pagina botao e home criada

--- src/app/pages/botao/botao.page.spec.ts
+++ src/app/pages/botao/botao.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-import { FolderPage } from './folder.page';
 
-describe('FolderPage', () => {
-  let component: FolderPage;
-  let fixture: ComponentFixture<FolderPage>;
+import { BotaoPage } from './botao.page';
+
+describe('BotaoPage', () => {
+  let component: BotaoPage;
+  let fixture: ComponentFixture<BotaoPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ FolderPage ],
-      imports: [IonicModule.forRoot(), RouterModule.forRoot([])]
+      declarations: [ BotaoPage ],
+      imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(FolderPage);
+    fixture = TestBed.createComponent(BotaoPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
