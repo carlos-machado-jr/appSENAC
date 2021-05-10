6d3f484bfcb98ec3445316a5988035ee19276c57
Técnico Informática <51121742019.1@senacrjedu.local>
2021-05-10 20:30:27 -0300

    funcao action sheet criada e refatorada

--- src/app/pages/actionsheet/actionsheet.page.ts
+++ src/app/pages/actionsheet/actionsheet.page.ts
@@ -1,4 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
+let buttons = [
+  {
+  text: "apagar",
+  icon: "trash",
+  handler: () => {
+    console.log("clicou em apagar")
+  }},
+  {
+    text: "Compartilhar",
+    icon: "share",
+    handler: () => {
+      console.log("clicou em compartilhar")
+  }},
+    {
+      text: "Exibir",
+      icon: "eye",
+      handler: () => {
+        console.log("clicou em exibir")
+  }},
+  {
+    text: "Favoritar",
+    icon: "heart",
+    handler: () => {
+      console.log("clicou em Favoritar")
+  }},
+  {
+    text: "Cancelar",
+    icon: "close",
+    handler: () => {
+      console.log("clicou em Cancelar")
+  }}
+
+
+]
 
 @Component({
   selector: 'app-actionsheet',
@@ -6,10 +42,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./actionsheet.page.scss'],
 })
 export class ActionsheetPage implements OnInit {
-
-  constructor() { }
+  
+  constructor(private actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
+  }
+
+  
+
+  async exibir(){
+    const actionSheet = await this.actionSheetController.create({
+      header: "fotos",
+      buttons: buttons
+    })
+    actionSheet.present();
   }
 
 }
